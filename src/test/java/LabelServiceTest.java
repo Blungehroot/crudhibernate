dde265fdcb60e502aa44cdb4621712b3333a7118
yevgeniy.shaidur <insight2pizzza@gmail.com>
2021-11-14 16:02:37 +0200

    finish with tests

---
+++ src/test/java/LabelServiceTest.java
@@ -0,0 +1,108 @@
+import com.crudhibernate.app.model.Label;
+import com.crudhibernate.app.repository.postgresrepository.LabelRepositoryImpl;
+import com.crudhibernate.app.service.serviceimpl.LabelServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class LabelServiceTest {
+
+    @InjectMocks
+    private LabelServiceImpl labelServiceImpl;
+
+    @Mock
+    private LabelRepositoryImpl labelRepository;
+
+    @Test
+    void saveLabel_shouldBeSuccess() {
+        Label label = new Label();
+        label.setId(1);
+        label.setName("Test label");
+
+        when(labelRepository.save(label)).thenReturn(label);
+
+        Label labelActual = labelRepository.save(label);
+
+        assertNotNull(label);
+        assertEquals(label, labelActual);
+
+        verify(labelRepository).save(ArgumentMatchers.eq(label));
+    }
+
+    @Test
+    void deleteLabel_shouldBeSuccess() {
+        Label label = new Label();
+        label.setId(1);
+        label.setName("Test label");
+
+        doNothing().when(labelRepository).deleteById(label.getId());
+
+        labelRepository.deleteById(label.getId());
+
+        verify(labelRepository).deleteById(label.getId());
+    }
+
+    @Test
+    void updateLabel_shouldBeSuccess() {
+        Label labelExist = new Label();
+        Label labelUpdated = new Label();
+
+        labelExist.setId(1);
+        labelExist.setName("Test label");
+
+        labelUpdated.setName("Updated");
+        labelUpdated.setId(1);
+
+        lenient().when(labelRepository.save(labelExist)).thenReturn(labelExist);
+        lenient().when(labelRepository.update(labelUpdated)).thenReturn(labelUpdated);
+
+        Label result = labelRepository.update(labelUpdated);
+
+        assertNotNull(result);
+        assertNotEquals(labelExist.getName(), result.getName());
+
+        verify(labelRepository).update(labelUpdated);
+    }
+
+    @Test
+    void getLabelById_shouldBeSuccess() {
+        Label label = new Label();
+        label.setId(1);
+        label.setName("Test label");
+
+        when(labelRepository.getById(label.getId())).thenReturn(label);
+
+        Label result = labelRepository.getById(label.getId());
+
+        assertNotNull(result);
+        assertEquals(label, result);
+
+        verify(labelRepository).getById(label.getId());
+    }
+
+    @Test
+    void getAll_shouldBeSuccess() {
+        Label label = new Label();
+        label.setId(1);
+        label.setName("Test label");
+
+        List<Label> labels = new ArrayList<>();
+        labels.add(label);
+
+        when(labelRepository.getAll()).thenReturn(labels);
+
+        assertEquals(labels, labelRepository.getAll());
+
+        verify(labelRepository).getAll();
+    }
+}
---
+++ src/test/java/PostServiceTest.java
@@ -0,0 +1,154 @@
+import com.crudhibernate.app.model.Label;
+import com.crudhibernate.app.model.Post;
+import com.crudhibernate.app.repository.postgresrepository.LabelRepositoryImpl;
+import com.crudhibernate.app.repository.postgresrepository.PostRepositoryImpl;
+import com.crudhibernate.app.service.serviceimpl.PostServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class PostServiceTest {
+
+    @InjectMocks
+    private PostServiceImpl postService;
+
+    @Mock
+    private LabelRepositoryImpl labelRepository;
+
+    @Mock
+    private PostRepositoryImpl postRepository;
+
+    @Test
+    void savePost_shouldBeSuccess() {
+        Label label = new Label();
+        Post post = new Post();
+        List<Label> labels = new ArrayList<>();
+
+        label.setId(1);
+        label.setName("Test label");
+
+        labels.add(label);
+
+        post.setId(1);
+        post.setContent("Test content");
+        post.setName("Test post");
+        post.setLabels(labels);
+
+        when(postRepository.save(post)).thenReturn(post);
+
+        Post postActual = postRepository.save(post);
+
+        assertNotNull(postActual);
+        assertEquals(post, postActual);
+
+        verify(postRepository).save(post);
+    }
+
+    @Test
+    void deletePost_shouldBeSuccess() {
+        Post post = new Post();
+
+        post.setId(1);
+        post.setContent("Test content");
+        post.setName("Test post");
+
+        doNothing().when(postRepository).deleteById(post.getId());
+
+        postRepository.deleteById(post.getId());
+
+        verify(postRepository).deleteById(post.getId());
+    }
+
+    @Test
+    void updateLabel_shouldBeSuccess() {
+        Post postExist = new Post();
+        Post postUpdated = new Post();
+        Label label = new Label();
+        List<Label> labels = new ArrayList<>();
+
+        label.setId(1);
+        label.setName("Test label");
+        labels.add(label);
+
+        postExist.setId(1);
+        postExist.setName("Test Content");
+        postExist.setContent("Test post content");
+        postExist.setLabels(labels);
+
+        postUpdated.setId(1);
+        postUpdated.setName("Test post update");
+        postUpdated.setContent("Test post content update");
+        postUpdated.setLabels(labels);
+
+        lenient().when(postRepository.save(postExist)).thenReturn(postExist);
+        lenient().when(postRepository.update(postUpdated)).thenReturn(postUpdated);
+
+        Post result = postRepository.update(postUpdated);
+
+        assertNotNull(result);
+        assertNotEquals(postExist.getName(), result.getName());
+
+        verify(postRepository).update(postUpdated);
+    }
+
+    @Test
+    void getPostById_shouldBeSuccess() {
+        Label label = new Label();
+        Post post = new Post();
+        List<Label> labels = new ArrayList<>();
+
+        label.setId(1);
+        label.setName("Test label");
+
+        labels.add(label);
+
+        post.setId(1);
+        post.setContent("Test content");
+        post.setName("Test post");
+        post.setLabels(labels);
+
+        when(postRepository.getById(post.getId())).thenReturn(post);
+
+        Post result = postRepository.getById(post.getId());
+
+        assertNotNull(result);
+        assertEquals(post, result);
+
+        verify(postRepository).getById(post.getId());
+    }
+
+    @Test
+    void getAll_shouldBeSuccess() {
+        Label label = new Label();
+        Post post = new Post();
+        List<Label> labels = new ArrayList<>();
+        List<Post> posts = new ArrayList<>();
+
+        label.setId(1);
+        label.setName("Test label");
+
+        labels.add(label);
+
+        post.setId(1);
+        post.setContent("Test content");
+        post.setName("Test post");
+        post.setLabels(labels);
+
+        posts.add(post);
+
+        when(postRepository.getAll()).thenReturn(posts);
+
+        assertEquals(posts, postRepository.getAll());
+
+        verify(postRepository).getAll();
+    }
+}
